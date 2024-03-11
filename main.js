7b014eabbb7fdf4af3745880ab920417cc7a3dc7
vuxlsu <156704588+vuxlsu@users.noreply.github.com>
2024-03-11 19:05:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+  
+        // діалогове вікно для виведення інформації на сторінку
+        // alert ("Lesson 1");
+        // діалогове вікно для виведення інформації на сторінку з можливістью підтвердення або відхилення
+        // confirm("Are you robbot ?");
+        // діалогове вікно для виведення інформація на сторінку з можливою ведення інфорації
+        // prompt("How are you ?")
+
+        // Виведення іноформації в консоль(тільки за розробником)
+        console.log('JS Lesson 1');
+
+
+
+        // let username = 'Alina';
+        // let age = 13;
+        // let likeIceCream = true;
+
+
+        // console.log(username);
+        // console.log(age);
+        // console.log(likeIceCream)
+
+        // let model = 'iPhone 11';
+        // let capacity = 128;
+        // let used = 95;
+        // let available = 33;
+        // let price = 23000;
+        // let color = 'black'
+        // let battery = '85%';
+        // let network = 'UA-KYIVSTAR';
+        // let cout photo  = 1975;
+        // let guarantee = 1;
+
+        // let model = 'porshe 911'
+        // let color = 'pink';
+        // let price = 1500000;
+        // let year = 2000;
+        // let goodcar = 'true';
+
+
+        // console.log(model);
+        // console.log(color)
+        // console.log(price)
+        // console.log(year)
+        // console.log(goodcar)
+
+
+
+        let num1 = prompt('Введи перше число');
+        let num2 = prompt('Ввведи друге число');
+
+
+        console.log(`${num1}+${num2}=${num1 + num2}`);
+        console.log(`${num1}-${num2}=${num1 - num2}`);
+        console.log(`${num1}*${num2}=${num1 * num2}`);
+        console.log(`${num1}/${num2}=${num1 / num2}`);
+        
+        
+
+      
+    
